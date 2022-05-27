ce8f34a92a3a6f0fb0d160d79a5932147dcf1074
Tomasz Wojciechowski <tomasz.wojciechowski@sollers.eu>
2022-05-27 15:12:49 +0200

    Completed module

--- src/main/java/com/h2/MortgageCalculator.java
+++ src/main/java/com/h2/MortgageCalculator.java
@@ -38,7 +38,13 @@ public class MortgageCalculator {
     }
 
     public static void main(String[] args) {
-        
+        long loanAmount = Long.parseLong(args[0]);
+        int termsInYears = Integer.parseInt(args[1]);
+        float annualRate = Float.parseFloat(args[2]);
+
+        MortgageCalculator calculator = new MortgageCalculator(loanAmount, termsInYears, annualRate);
+        calculator.calculateMonthlyPayment();
+        System.out.println(calculator.toString());
     }
 
     //    long P = loanAmount;
