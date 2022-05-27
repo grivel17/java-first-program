dc2b46cfd91dd43b2bc2479f2122b3533c9a82ff
Tomasz Wojciechowski <tomasz.wojciechowski@sollers.eu>
2022-05-27 15:17:44 +0200

    next two

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -1,3 +1,6 @@
 public class Finance {
+    public final String BEST_LOAN_RATES = "bestLoanRates";
+    public final String SAVINGS_CALCULATOR = "savingsCalculator";
+    public final String MORTGAGE_CALCULATOR = "mortgageCalculator";
 
 }
